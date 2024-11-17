c721e0119efdf75d99165b5d56e4fcd0660b892a
KylaF8 <20098155@mail.wit.ie>
2024-11-17 16:46:58 +0000

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -32,6 +32,10 @@ export class EDAAppStack extends cdk.Stack {
       displayName: "New Image topic",
     }); 
 
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
   // Lambda functions
 
   const processImageFn = new lambdanode.NodejsFunction(
@@ -45,28 +49,54 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
   imagesBucket.addEventNotification(
     s3.EventType.OBJECT_CREATED,
     new s3n.SnsDestination(newImageTopic)  // Changed
 );
 
- // SQS --> Lambda
-  const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
-    batchSize: 5,
-    maxBatchingWindow: cdk.Duration.seconds(5),
-  });
+newImageTopic.addSubscription(
+  new subs.SqsSubscription(imageProcessQueue)
+);
 
-  processImageFn.addEventSource(newImageEventSource);
+const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
+  batchSize: 5,
+  maxBatchingWindow: cdk.Duration.seconds(5),
+});
 
-  // Permissions
+const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+  batchSize: 5,
+  maxBatchingWindow: cdk.Duration.seconds(5),
+}); 
+
+processImageFn.addEventSource(newImageEventSource);
+
+mailerFn.addEventSource(newImageMailEventSource);
+
+newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+
+mailerFn.addToRolePolicy(
+  new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    actions: [
+      "ses:SendEmail",
+      "ses:SendRawEmail",
+      "ses:SendTemplatedEmail",
+    ],
+    resources: ["*"],
+  })
+);
+
 
   imagesBucket.grantRead(processImageFn);
 
-  newImageTopic.addSubscription(
-    new subs.SqsSubscription(imageProcessQueue)
-  );
-
-  // Output
   
   new cdk.CfnOutput(this, "bucketName", {
     value: imagesBucket.bucketName,
